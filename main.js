87d0dec3989e8a6e0b7f0d79a30582805ee3a352
bgrinste <bgrinste@netapp.com>
2021-09-24 03:37:08 +0300

    add spinner, description, dynamic errMsg

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const ERROR_MSGS = {
 }
 
 
-
 function populateBookCards(books) {
     const cardList = document.getElementById('main');
 
@@ -24,34 +23,60 @@ function populateBookCards(books) {
             copyTemp.querySelector('.by i').textContent = 'by';
             copyTemp.querySelector('.book-author').textContent = `${book.volumeInfo.authors.join(' ,')}`;
         }
+        if (book.volumeInfo.description) {
+            copyTemp.querySelector('.book-desc').textContent = `${book.volumeInfo.description}`;
+        }
+        else {
+            copyTemp.querySelector('.book-desc').textContent = `No description`
+        }
 
         cardList.appendChild(copyTemp);
     });
 }
 
+async function searchBooks() {
+    const searchTerm = document.querySelector('#searchTerm').value;
+    const errMsgContainer = document.querySelector('#errMsg');
+
+    if (searchTerm) {
+        activateSpinner();
+        errMsgContainer.style.display = 'none';
+        removeBookResults();
+
+        const data  = await fetch(BASE_URL + searchTerm);
+        const books = await data.json();
+        clearSpinner();
+        if (books.items && books.items.length > 0) {
+            populateBookCards(books.items);
+        }
+        else {
+            showErrorMsg(errMsgContainer, ERROR_MSGS.NO_RESULTS);
+        }
+    }
+    else {
+        showErrorMsg(errMsgContainer, ERROR_MSGS.NO_SEARCH_TERM);
+    }
+}
 
 function removeBookResults() {
     document.querySelector('#main').innerHTML = '';
 }
 
-async function searchBooks() {
-    const searchTerm = document.querySelector('#searchTerm').value;
-    const dangerMsgDiv = document.querySelector('.danger');
-
-    if (searchTerm) {
-        dangerMsgDiv.style.display = 'none';
-        removeBookResults();
-
-        const data  = await fetch(BASE_URL + searchTerm);
-        const books = await data.json();
-        if (books.items && books.items.length > 0) {
-            populateBookCards(books.items);
-        }
-    }
-    else {
-        dangerMsgDiv.style.display = 'block';
-        setTimeout(() => dangerMsgDiv.style.display = 'none', 3000);
-
-    }
+function showErrorMsg(errMsgContainer, content) {
+    errMsgContainer.textContent   = content;
+    errMsgContainer.style.display = 'flex';
+    setTimeout(() => errMsgContainer.style.display = 'none', 3000);
 }
+
+function activateSpinner() {
+    document.querySelector('.lds-ring').style.display = 'inline-block';
+}
+
+function clearSpinner() {
+    document.querySelector('.lds-ring').style.display = 'none';
+
+}
+
+
+
 
